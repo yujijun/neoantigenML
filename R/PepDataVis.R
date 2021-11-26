311df6189a06bde5e26d0ae04def897df9d11846
yujijun <jijunyu140@gamil.com>
2021-11-26 13:12:02 +0800

    change name from pepLength into pepVisLength

--- R/PepDataVis.R
+++ R/PepDataVis.R
@@ -5,15 +5,15 @@
 #' @examples
 #' pepSeq <- Neodataset$wild_Peptide
 #' pepLength(pepSeq = pepSeq)
-#' 
-pepLength <- function(pepSeq){
-  pepSeq %>% as.data.frame() %>% 
-    mutate(pep_length = stringr::str_length(pepSeq)) %>% 
-    ggplot(aes(x = pep_length)) + 
-    geom_bar(position = "stack") + 
-    ggtitle("Length statistics of all peptides") + 
-    theme(plot.title = element_text(hjust = 0.5,face = "bold")) -> 
-    ggbar 
+#'
+pepVisLength <- function(pepSeq){
+  pepSeq %>% as.data.frame() %>%
+    mutate(pep_length = stringr::str_length(pepSeq)) %>%
+    ggplot(aes(x = pep_length)) +
+    geom_bar(position = "stack") +
+    ggtitle("Length statistics of all peptides") +
+    theme(plot.title = element_text(hjust = 0.5,face = "bold")) ->
+    ggbar
   return(ggbar)
 }
 
