cdf65ac8f6ff2376803aa00a1befff738805b06e
yujijun <jijunyu140@gamil.com>
2021-11-28 22:28:02 +0800

    Add PerformanceEva function

---
+++ R/HyperparameterTuning.R
@@ -0,0 +1,43 @@
+# BASIC INFO ---------------------------
+##
+## Script name: HyperparameterTuning.R
+##
+## Purpose of script: Model tuning
+##
+## Author: JijunYu
+##
+## Date Created: 2021-11-28
+## Update Date:
+##
+## Copyright (c) Jijun Yu, 2021
+## Email: jijunyuedu@outlook.com
+##
+## Notes:
+## Different machine learning method have different tunning parameter
+#--------------Main -----------------------
+Tunning <- function(dataset = MLtestData,
+                    target = "judge",
+                    taskid = "MLtest",
+                    learnmethod = "classif.rpart"){
+  require("mlr3verse")
+  require("FSelectorRcpp")
+  task = TaskClassif$new(id=taskid, backend=dataset, target = target)
+  learner = lrn(learnmethod)
+  search_space = ps(
+    cp = p_dbl(lower = 0.001, upper = 0.1),
+    minsplit = p_int(lower = 1, upper = 10))
+  hout = rsmp("holdout")
+  measure = msr("classif.ce")
+  evals20 = trm("evals", n_evals = 20)
+  instance = TuningInstanceSingleCrit$new(
+    task = task,
+    learner = learner,
+    resampling = hout,
+    measure = measure,
+    search_space = search_space, # different model need different search_space
+    terminator = evals20
+  )
+  tuner = tnr("grid_search", resolution = 5)
+  tuner$optimize(instance)
+  return(tuner)
+}
---
+++ R/PerformanceEva.R
@@ -0,0 +1,94 @@
+# BASIC INFO ---------------------------
+##
+## Script name:BenchmarkEva.R
+##
+## Purpose of script: Evaluation different methods by benchmark methods.
+##
+## Author: JijunYu
+##
+## Date Created: 2021-11-28
+## Update Date:
+##
+## Copyright (c) Jijun Yu, 2021
+## Email: jijunyuedu@outlook.com
+##
+## Notes:
+##
+#--------------Main:Performance Evaluation --------------------
+#' Title Performance evaluation of single machine learning model.
+#'
+#' @param task A task object created by mlr3
+#' @param leaner A learner object created by mlr3
+#' @param resamplemethod A resample object created by mlr3
+#' @param bmr_small A single benchmark object such as  bmr_small = bmr$clone()$filter(task_id = tasks[1])
+#'
+#' @return A list with all performance results.
+#' @export
+#'
+#' @examples
+#' data("Sonar", package = "mlbench")
+#' task = as_task_classif(Sonar, target = "Class", positive = "M")
+#' learner = lrn("classif.rpart", predict_type = "prob")
+#' PerformanceEva(task = task,learner = learner)
+PerformanceEva <- function(task,
+                           learner,
+                           resamplemethod = rsmp("cv",folds =3),
+                           bmr_small){
+  require(mlr3verse)
+  if(is.empty(bmr_small) == TRUE){
+    rr = resample(task, learner, resamplemethod, store_models = TRUE)
+  }else{
+    rr = bmr_small
+  }
+  autoplot(rr, measure = msr("classif.auc"))
+  roc = autoplot(rr,type = "roc")
+  roc.best = autoplot(roc$prediction(), type = "roc")
+  prc = autoplot(rr,type = "prc")
+  prediction = autoplot(rr, type = "prediction")
+  Allperformance = list(roc,roc.best,prc,prediction)
+  return(Allperformance)
+}
+
+#--------------Main:benchmark calculation-----------------------
+#' Title Benchmark calculation
+#' In particular unique combination of Task,Learner and Resampling.
+
+#' @param tasks A vector of task
+#' @param learners A vector of learners
+#' @param reamplings A resamplings object. detail could be found in example
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' design = benchmark_grid(
+#' tasks = tsks(c("spam", "german_credit", "sonar")),
+#' learners = lrns(c("classif.ranger", "classif.rpart", "classif.featureless"),
+#'                predict_type = "prob", predict_sets = c("train", "test")),
+#' resamplings = rsmps("cv", folds = 3)
+BenchmarkEva <- function(tasks,
+                         learners,
+                         reamplings){
+  require("mlr3verse")
+  require("data.table")
+  design = benchmark_grid(
+    tasks = tasks,
+    learners = learners,
+    resamplings = resamplings
+  )
+  bmr = benchmark(design)
+  measures = list(
+    msr("classif.auc", predict_sets = "train", id = "auc_train"),
+    msr("classif.auc", id = "auc_test")
+  )
+  tab = bmr$aggregate(measures)
+  ranks = tab[, .(learner_id, rank_train = rank(-auc_train), rank_test = rank(-auc_test)), by = task_id]
+  ranks = ranks[, .(mrank_train = mean(rank_train), mrank_test = mean(rank_test)), by = learner_id]
+  bmr.barplot = autoplot(bmr) + ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 45, hjust = 1))
+  # bmr_small = bmr$clone()$filter(task_id = tasks[1])
+  # bmr_roc = autoplot(bmr_small, type = "roc")
+  # Extracting ResampleResults:https://mlr3book.mlr-org.com/perf-eval-cmp.html
+  Allresult <- list(bmr,tab,ranks,bmr.barplot)
+  return(Allresult)
+}
+
